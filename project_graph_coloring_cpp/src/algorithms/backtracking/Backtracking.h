8ac110daacf8b89930a5b1b6367cd374622a6044
Hà Nguyễn <helotuiyenha@gmail.com>
2025-11-11 12:50:37 +0700

    Update Backtracking.h

--- project_graph_coloring_cpp/src/algorithms/backtracking/Backtracking.h
+++ project_graph_coloring_cpp/src/algorithms/backtracking/Backtracking.h
@@ -1,11 +1,9 @@
 #pragma once
+#include "../include/Graph.h"
 #include <vector>
-#include "../graph_representation/Graph.h"
 
 class BacktrackingSolver {
 public:
-    static std::vector<int> solve(const Graph& graph, int maxColors);
-private:
-    static bool isSafe(int v, const Graph& graph, const std::vector<int>& colors, int c);
-    static bool graphColoringUtil(const Graph& graph, int m, std::vector<int>& colors, int v);
+    static std::vector<int> solve(const Graph& g, int maxColors);
 };
+
