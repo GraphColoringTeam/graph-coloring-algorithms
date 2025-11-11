0e71f8a90487139f535f4bb5f6a1f9f2fde1f8da
Hà Nguyễn <helotuiyenha@gmail.com>
2025-11-11 12:52:56 +0700

    Update CP_Solver.cpp

--- project_graph_coloring_cpp/src/algorithms/constraint_programming/CP_Solver.cpp
+++ project_graph_coloring_cpp/src/algorithms/constraint_programming/CP_Solver.cpp
@@ -1,37 +1,29 @@
 #include "CP_Solver.h"
-#include <iostream>
-
-// Nếu bạn đã cài OR-Tools, bỏ comment phần dưới và include thư viện:
 #include "ortools/sat/cp_model.h"
-
+#include <iostream>
 using namespace operations_research;
 using namespace sat;
 
-std::vector<int> CPSolver::solve(const Graph& graph, int maxColors) {
+std::vector<int> CPSolver::solve(const Graph& g, int maxColors) {
     CpModelBuilder model;
+    int n = g.numVertices();
 
-    std::vector<IntVar> colorVars;
-    for (int i = 0; i < graph.numVertices(); ++i) {
-        colorVars.push_back(model.NewIntVar(Domain(1, maxColors)).WithName("color_" + std::to_string(i)));
+    std::vector<IntVar> color;
+    for (int i = 0; i < n; ++i)
+        color.push_back(model.NewIntVar(Domain(1, maxColors)).WithName("c" + std::to_string(i)));
+
+    for (int u = 0; u < n; ++u)
+        for (int v : g.getAdj(u))
+            if (u < v)
+                model.AddNotEqual(color[u], color[v]);
+
+    Model m;
+    CpSolverResponse resp = SolveCpModel(model.Build(), &m);
+
+    std::vector<int> result(n, 0);
+    if (resp.status() == CpSolverStatus::OPTIMAL || resp.status() == CpSolverStatus::FEASIBLE) {
+        for (int i = 0; i < n; ++i)
+            result[i] = SolutionIntegerValue(resp, color[i]);
     }
-
-    // Ràng buộc: các đỉnh kề nhau không được cùng màu
-    for (int u = 0; u < graph.numVertices(); ++u) {
-        for (int v : graph.getAdj(u)) {
-            if (u < v) model.AddNotEqual(colorVars[u], colorVars[v]);
-        }
-    }
-
-    Model cpModel;
-    const CpSolverResponse response = SolveCpModel(model.Build(), &cpModel);
-
-    std::vector<int> colors(graph.numVertices(), 0);
-    if (response.status() == CpSolverStatus::OPTIMAL || response.status() == CpSolverStatus::FEASIBLE) {
-        for (int i = 0; i < graph.numVertices(); ++i)
-            colors[i] = SolutionIntegerValue(response, colorVars[i]);
-    } else {
-        std::cerr << "Không tìm được nghiệm khả thi.\n";
-    }
-
-    return colors;
+    return result;
 }
